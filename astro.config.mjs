f803022b407c3524134ab779c3c1451c89308c09
Antonio <jkingstom@gmail.com>
2025-07-02 09:45:29 +1000

    Set base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: 'static',
+  base: '/portfolio2024', // 👈 important
 });
