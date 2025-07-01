d801fe56f7677e4fb958eaf2cd3c3a128e8b65fe
tonhaoln <tonhaoln@users.noreply.github.com>
2025-07-01 23:35:00 +0000

    deploy: d2fad228f4db55834a546cf04003eca45e1f89a3

---
+++ _astro/hoisted.BBpiKzQa.js
@@ -0,0 +1,1 @@
+import"./hoisted.C8fJdJU1.js";const t=()=>{const e=document.createElement("script");e.src="//embed.typeform.com/next/embed.js",document.body.appendChild(e)};typeof document<"u"&&t();
